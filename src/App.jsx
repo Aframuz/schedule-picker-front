49272d4b6842ad91487c1147e31fa137d9700cb1
aframuz <aframuz@tuta.io>
2024-07-30 17:36:03 -0400

    feat: init repo

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import FileContent from './FileContent';
+import './App.css';
+
+function App() {
+  return(
+    <div className='App'>
+      <h1>File Content</h1>
+      <FileContent/>
+    </div>
+  );
+};
+
+export default App
---
+++ src/FileContent.jsx
@@ -0,0 +1,28 @@
+import React, {useState, useEffect} from 'react';
+
+const FileContent = () => {
+    const [content, setContent ] = useState('');
+
+    useEffect(()=> {
+        const fetchFileContent = async () => {
+            try {
+                const response = await fetch('./public/schedules.txt');
+                const text = await response.text();
+                setContent(text)
+            } catch( error){
+                console.error('Error fetching file content', error)
+            }
+        };
+
+        fetchFileContent();
+    }, []);
+
+    return (
+        <div>
+            <h2>File Content</h2>
+            <pre>{content}</pre>
+        </div>
+    );
+};
+
+export default FileContent
